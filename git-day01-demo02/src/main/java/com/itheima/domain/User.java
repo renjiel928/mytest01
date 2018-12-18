1e2bb6ff5f6944b5ef312c2567f2c4c14f563c4f
renjiel <renjiel820@gmail.com>
2018-12-18 21:32:02 +0800

    demo01

---
+++ git-day01-demo02/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @program: git
+ * @description:
+ * @author: Element
+ * @create: 2018-12-18 21:27
+ **/
+
+public class User {
+    private Integer id;
+    private String name;
+}
